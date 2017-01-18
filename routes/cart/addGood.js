7066ed7f61c3e70bc4d670fbdb70c0f99c08eedb
Golovkina Valentina <gvalya.02@gmail.com>
2017-01-18 17:46:08 +0300

    убрала параметры из запроса добавления товара

--- routes/cart/addGood.js
+++ routes/cart/addGood.js
@@ -2,36 +2,39 @@
  * Created by Валентина on 14.01.2017.
  */
 var Cart = require('model/cart').Cart;
+var Good = require('model/good').Good;
 var async = require('async');
 exports.add = function (req, res, next) {
     var size = req.query.size;
     var goodID =req.query.goodID;
     var userID = req.session.user;
-    var goodname = req.query.goodname;
-    var category =req.query.category;
-    var price = req.query.price;
-    var img = req.query.img;
     if(userID != undefined) {
         if(size!=undefined) {
             async.waterfall([
                 function(callback) {
                     Cart.findOne({userID: userID, goodID:goodID,size:size}, callback);
                 },
-                function(cart, next) {
+                function(cart, callback) {
                     if (cart) {
                         cart.count=cart.get('count')+1;
+                        callback(null,cart);
                     }else {
-                        cart = new Cart({userID: userID,goodID: goodID, goodname:goodname, category:category, price:price, img: img, size: size, count: '1'});
+                        Good.findOne({_id:goodID},  function (err, good){
+                            cart = new Cart({userID: userID,goodID: goodID, goodname:good.get('goodname'), category:good.get('category'), price:good.get('price'), img: good.get('img'), size: size, count: '1'});
+                            callback(null,cart);
+                        });
                     }
+
+                },
+                function(cart) {
                     cart.save(function(err) {
                         if (err) { next(500); }
-                        else { res.status(200); }
+                        else { res.status(200).end(); }
                     });
                 }
             ], next);
         }
-        else { next(401); }
+        else { next(401).end(); }
     }
-    else { next(402); }
-    res.end();
+    else { next(402).end(); }
 };
